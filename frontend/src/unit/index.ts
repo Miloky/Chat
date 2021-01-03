0cd0e63b88de7d948f683d9dc18da0578d466500
Miloky <monmorti@gmail.com>
2021-01-03 06:32:11 +0200

    refactor

--- frontend/src/unit/index.ts
+++
@@ -1,9 +0,0 @@
-export const enumerable = (start: number, end: number): number[] => {
-  let index = 0;
-  const result: number[] = [];
-  while (start <= end) {
-    result[index++] = start++;
-  }
-
-  return result;
-};

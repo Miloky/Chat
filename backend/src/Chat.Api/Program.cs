a0288afcf8e559b39a0c36ae8cd2a868e7aa0280
Miloky <monmorti@gmail.com>
2021-01-05 14:36:41 +0200

    Serilog

--- backend/src/Chat.Api/Program.cs
+++ backend/src/Chat.Api/Program.cs
@@ -1,5 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace Chat.Api
 {
@@ -7,11 +12,30 @@ namespace Chat.Api
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostBuilderContext, config) =>
+                {
+                    var env = hostBuilderContext.HostingEnvironment.EnvironmentName;
+                    Console.WriteLine($"HostingEnvironment.EnvironmentName: {env}");
+                    config.AddJsonFile("appsettings.json", false, true)
+                        .AddJsonFile($"appsettings.{env}.json", true, true)
+                        .AddJsonFile("appsettings.Local.json", true, true)
+                        .AddEnvironmentVariables();
+                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
